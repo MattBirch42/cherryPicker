d1b514b10a418130b58c5114d9da6d43dc254f6d
Matt Birch <matt---@hotmail.com>
2025-09-24 05:01:38 -0700

    updated package

---
+++ app.R
@@ -0,0 +1,11 @@
+# app.R
+library(cherryPicker)
+
+# Launch the app
+cherryPicker:::run_cherry_picker()
+
+# rsconnect::deployApp(
+#   appDir = "C:/Users/mbirch/Documents/cherryPicker_support/",          # current folder
+#   appName = "cherry_picker",   # choose a name
+#   account = "mattbirch"
+# )
