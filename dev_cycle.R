a78d4a48aa86ba3030834d0f206cd3238d05aebd
Matt Birch <matt---@hotmail.com>
2025-10-01 01:48:39 -0700

    a little more in the dev file

--- dev_cycle.R
+++ dev_cycle.R
@@ -19,3 +19,13 @@ if (nzchar(commit_msg)) {
   message("⚠️  No commit message entered. Skipping commit.")
 }
 system("git push")
+
+
+# rsconnect::accounts()
+# setwd("C:/Users/mbirch/Documents/cherryPicker_support")
+# 
+# rsconnect::deployApp(
+#   appDir = ".",                     # current directory
+#   appName = "cherryPicker",          # pick an app name
+#   account = "mattbirch"      # same as in setAccountInfo
+# )
